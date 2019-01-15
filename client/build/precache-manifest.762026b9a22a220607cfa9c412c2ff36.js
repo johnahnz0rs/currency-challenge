1b70380de35b5b55b340f7d4902a42a0cdecf187
John Ahn <john@launchpartner.co>
2019-01-15 13:22:50 -0800

    adding client/build to git

---
+++ client/build/precache-manifest.762026b9a22a220607cfa9c412c2ff36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbbb55e2a66f42f525f1",
+    "url": "/static/js/main.bbbb55e2.chunk.js"
+  },
+  {
+    "revision": "ce1cf789479dc6b15c66",
+    "url": "/static/js/1.ce1cf789.chunk.js"
+  },
+  {
+    "revision": "bbbb55e2a66f42f525f1",
+    "url": "/static/css/main.1822377c.chunk.css"
+  },
+  {
+    "revision": "167bc0bada32e3014910a14f00fe9e8a",
+    "url": "/index.html"
+  }
+];
